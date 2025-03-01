307c985cc7a5b405766a61055868d4f70692a79b
timothyaxel <timothyaxel@student.tudelft.nl>
2025-03-01 18:46:37 +0100

    Added info variable and made wait() on PromptSession concrete

--- agents1/eventUtils.py
+++ agents1/eventUtils.py
@@ -3,12 +3,21 @@ from abc import abstractmethod
 
 # Class to manage which prompt was last encountered by the bot
 class PromptSession:
-    def __init__(self, bot, ttl=100):
+    def __init__(self, bot, info, ttl=100):
         self.bot = bot
-        self.ttl = ttl
+        self.info = info # Store info about the current object (used for directing removal)
+        self.ttl = ttl # Store
 
-    @abstractmethod
-    def wait(self): pass
+    def wait(self):
+        if self.ttl % 5 == 0 and self.ttl > 0:
+            print("ttl:", self.ttl)
+
+        if self.ttl > 0:
+            self.ttl -= 1
+        if self.ttl == 0:
+            return self.on_timeout()
+
+        return None, {}
     @abstractmethod
     def on_timeout(self): pass
 
