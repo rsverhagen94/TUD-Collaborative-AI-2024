d28ece9336ed1245f4bc9276a9a51bc652da8b5f
Maciej Mejer <maciek@DESKTOP-P8VNVT4.>
2025-03-08 22:37:07 +0100

    make it possible to plot multiple games

--- beliefs/generate_plot.py
+++ beliefs/generate_plot.py
@@ -1,6 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
 
+files = ['actions2.csv', 'actions3.csv']
+
 timestamps = []
 competency_rescue_severelyInjured = []
 competency_rescue_mildlyInjured = []
@@ -8,31 +10,36 @@ competency_arrival = []
 willingness_search = []
 willingness_response = []
 willingness_rescue = []
-score = []
-completeness = []
+scores = []
+completenesses = []
 
-ticks = []
 name = ''
 
-with open('actions.csv', 'r') as file:
-    reader = csv.DictReader(file, delimiter=';')
+for i in range(len(files)):
+    with open(files[i], 'r') as file:
+        reader = csv.DictReader(file, delimiter=';')
 
-    for row in reader:
-        name = row['name']
-        competency_rescue_severelyInjured.append(float(row['competency_rescue_severelyInjured']))
-        competency_rescue_mildlyInjured.append(float(row['competency_rescue_mildlyInjured']))
-        competency_arrival.append(float(row['competency_arrival']))
-        willingness_search.append(float(row['willingness_search']))
-        willingness_response.append(float(row['willingness_response']))
-        willingness_rescue.append(float(row['willingness_rescue']))
+        scores.append([])
+        completenesses.append([])
 
-        score.append(float(row['score']))
-        completeness.append(float(row['completeness']))
-        ticks.append(float(row['tick_nr']))
+        for row in reader:
+            name = row['name']
+            competency_rescue_severelyInjured.append(float(row['competency_rescue_severelyInjured']))
+            competency_rescue_mildlyInjured.append(float(row['competency_rescue_mildlyInjured']))
+            competency_arrival.append(float(row['competency_arrival']))
+            willingness_search.append(float(row['willingness_search']))
+            willingness_response.append(float(row['willingness_response']))
+            willingness_rescue.append(float(row['willingness_rescue']))
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8), sharex=True)
+            scores[i].append(float(row['score']))
+            completenesses[i].append(float(row['completeness']))
 
+ticks = range(len(competency_rescue_severelyInjured))
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
 fig.suptitle(name, fontsize=14)
+
+# plot 1
 
 ax1.plot(ticks, competency_rescue_severelyInjured, label='competency_rescue_severelyInjured')
 ax1.plot(ticks, competency_rescue_mildlyInjured, label='competency_rescue_mildlyInjured')
@@ -46,14 +53,22 @@ ax1.set_ylim(-1, 1)
 ax1.legend()
 ax1.grid(True)
 
-ax2.plot(ticks, score, label='score')
-ax2.plot(ticks, completeness, label='completeness')
+# plot 2
+
+ax3 = ax2.twinx()
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+for i in range(len(files)):
+    ax2.plot(range(len(scores[i])), scores[i], label='score_game' + str(i+1), color=colors[i])
+    ax3.plot(range(len(scores[i])), completenesses[i], label='completeness_game' + str(i+1), color=colors[i+4])
 
 ax2.set_title('Progress')
-ax1.set_ylim(0, 1)
 ax2.set_xlabel('Tick')
 ax2.legend()
 ax2.grid(True)
+
+ax3.set_ylim(0, 1)
+ax3.legend()
 
 plt.tight_layout()
 plt.show()
